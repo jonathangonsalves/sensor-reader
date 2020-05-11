e691299b39f5210c037953a33ac13ffe42aee021
jonathangonsalves <jonathangonsalves@outlook.com>
2020-05-11 20:05:36 -0300

    [ADD] Script lê e escreve na máquina final o resultado
    Signed-off-by: jonathangonsalves <jonathangonsalves@outlook.com>

--- reader/reader.py
+++ reader/reader.py
@@ -1,5 +1,6 @@
 from serial import Serial
-import time
+import time, datetime
+import json
 
 # TODO
 # 1. Verificar o porque do delay ao ler os primeiros valores
@@ -18,6 +19,11 @@ class Potenciometro():
     def read(self):
         return self.ser.readline().decode().rstrip()
 
+    def save(self, final_result):
+        with open('final.json', 'w') as outfile:
+            json.dump(final_result, outfile)
+        return self
+
     def close(self):
         try:
             self.ser.close()
@@ -30,16 +36,31 @@ def main():
     pot = Potenciometro('/dev/ttyACM0')
     time.sleep(2)
 
-    for i in range(100):
-        val_atual = pot.read()
-        print(val_atual)
-        try:
-            data.append(val_atual)
-        except:
-            data.append(" ")
-        time.sleep(0.1)            # wait (sleep) 0.1 seconds
+    sensor_one = {}
+    sensor_two = {}
+    print("Setting up...")
+    for i in range(120):
+        if i < 50:
+            val_atual = pot.read()
+        else:
+            val_atual = pot.read()
+            #print(val_atual)
+            try:
+                if val_atual.startswith("s1"):
+                    data.append(val_atual[4:])
+                    sensor_one[str(datetime.datetime.now())] = val_atual[4:]
+                elif val_atual.startswith("s2"):
+                    data.append(val_atual[4:])
+                    sensor_two[str(datetime.datetime.now())] = val_atual[4:]
+                else:
+                    continue
+            except:
+                data.append(" ")
+            time.sleep(0.1)
 
     print(data)
+    final = {'sensor_one':sensor_one,'sensor_two':sensor_two}
+    pot.save(final)
     pot.close()
 
 if  __name__ == "__main__":
