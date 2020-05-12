c82db10211ba5429e70d927b07b7b65b1d0e10fa
jonathangonsalves <jonathangonsalves@outlook.com>
2020-05-11 21:14:18 -0300

    [NEW] Codificador, arquivos arduino e fixes
    Signed-off-by: jonathangonsalves <jonathangonsalves@outlook.com>

--- reader/reader.py
+++ reader/reader.py
@@ -1,10 +1,12 @@
+import json, hashlib, time, datetime
 from serial import Serial
-import time, datetime
-import json
 
 
-class Potenciometro():
-    def __init__(self, port):    
+
+class SensorReader():
+
+    def __init__(self, port):   
+
         self.port = port
         self.final = {'sensor_one':{},'sensor_two':{}}
         self.sensor_one = {}
@@ -17,22 +19,33 @@ class Potenciometro():
             print("Houve um erro ao se conectar com a porta!")
             quit()
 
+
+    def validData(self, data):
+        data = data.split('+')
+
+        m = hashlib.md5(data[0].encode())
+        if(data[1].replace("\r\n","") == m.hexdigest()):
+            return data[0]
+
+
     def read(self):
         return self.ser.readline().decode().rstrip()
 
+
     def save(self, sensor, value):
         self.final = {}
+
+        decode_value = self.validData(value)
+
         if sensor == 1:
-            self.sensor_one[str(datetime.datetime.now())] = value
+            self.sensor_one[str(datetime.datetime.now())] = decode_value
         else:
-            self.sensor_two[str(datetime.datetime.now())] = value
+            self.sensor_two[str(datetime.datetime.now())] = decode_value
         
         self.final = {'sensor_one':self.sensor_one,'sensor_two':self.sensor_two}
 
         with open('final.json', 'w') as outfile:
             json.dump(self.final, outfile)
-
-        return self
 
     def close(self):
         try:
@@ -43,7 +56,8 @@ class Potenciometro():
 
 def main():
 
-    pot = Potenciometro('/dev/ttyACM0')
+    # Setar porta serial do Notebook
+    pot = SensorReader('/dev/ttyACM0')
     time.sleep(2)
 
     print("Setting up...")
@@ -52,7 +66,6 @@ def main():
             val_atual = pot.read()
         else:
             val_atual = pot.read()
-            #print(val_atual)
             try:
                 pot.save(1, val_atual[4:]) if val_atual.startswith("s1") else pot.save(2, val_atual[4:])
             except:
---
+++ reader/sensor_reader/sensor_reader.ino
@@ -0,0 +1,46 @@
+// potentiometer_read.ino
+#include "MD5.h"
+
+
+int sensorOne = A0;
+int sensorTwo = A1;
+int sensorValueOne;
+int sensorValueTwo;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  sensorValueOne = analogRead(sensorOne);
+  String code_sensorValueOne = codificar(String(sensorValueOne));
+  Serial.print("s1: ");
+  Serial.println(code_sensorValueOne);
+
+  
+  delay(100);
+
+  sensorValueTwo = analogRead(sensorTwo);
+  String code_sensorValueTwo = codificar(String(sensorValueTwo));
+  Serial.print("s2: ");
+  Serial.println(code_sensorValueTwo);
+
+  delay(100);
+}
+
+
+String codificar(String value)
+{
+  
+  char *ldrValueChar = (char*) malloc(sizeof(char)*value.length()+1);
+  value.toCharArray(ldrValueChar, value.length()+1);
+  
+  unsigned char* hash= MD5::make_hash(ldrValueChar);
+  char *md5str = MD5::make_digest(hash, 16);
+  String data = String(value) + "+" + (char *)md5str;
+  free(hash);
+  return data;
+  
+}
